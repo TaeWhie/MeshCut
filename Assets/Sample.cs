9a9788b155a656149d1fe11741965078497237c6
TaeWhie <xognl427@naver.com>
2022-03-23 19:28:41 +0900

    OutsideCut

---
+++ Assets/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshCutter.Slicer(gameObject, new Vector3(1, 0, 0), new Vector3(0,0.25f,0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
